a6f51c7e4de226b895711e5e5c808d9a649c29a1
damstrivandrum <aajalms@gmail.com>
2026-07-13 23:26:40 +0530

    Update config.js

--- config.js
+++ config.js
@@ -27,7 +27,7 @@ window.EXAM_TZ = "Asia/Kolkata";
 
 // ✅ Fixed exam window in IST
 window.EXAM_START_IST = "2026-07-12T10:00:00+05:30";
-window.EXAM_END_IST   = "2026-07-12T22:00:00+05:30";
+window.EXAM_END_IST   = "2026-07-15T22:00:00+05:30";
 
 // ✅ Parsed milliseconds
 window.EXAM_START_MS = Date.parse(window.EXAM_START_IST);
